22d5c50e16964a2299c67cda4db2b6b48271baa5
OksanaSarkissian <osarkissian@esimed.fr>
2024-04-22 14:55:52 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={ <App />} />
+        {/* <Route path="/lorem" element={ <Home />} />
+        <Route path="/img" element={ <ImgTp2 />} />
+        <Route path="/dog" element={ <ApiSearch />} /> */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
